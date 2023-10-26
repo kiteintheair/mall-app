cf8ed82add08c28794a3a0893729eade8ac924d6
sun <975993217@qq.com>
2023-10-26 20:33:43 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+import '@/assets/css/reset.less'
+
+// 导入注册antdesign
+import 'ant-design-vue/dist/antd.css'
+import Antd from 'ant-design-vue'
+Vue.use(Antd)
 
 new Vue({
   router,
